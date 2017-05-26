cca90a56dc06d5265ce80c279a38c227f00516b9
juan sebastian cruz torrico <jsebas.ct@gmail.com>
2017-05-26 01:09:55 -0300

    first commit react components

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Button from 'react-toolbox/lib/button/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <Button label="Save"/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
